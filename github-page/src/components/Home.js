42729eb011b101cd86cc52e529f5969d30cf302c
SamBarbeau <samueljosephbarbeau@gmail.com>
2024-09-26 00:49:01 -0500

    add initial react files

---
+++ github-page/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold">Welcome to My GitHub Page!</h1>
+      <p className="mt-4">Here you'll find my projects and more.</p>
+    </div>
+  );
+};
+
+export default Home;
